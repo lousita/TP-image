68a48dca31065e1b63141418f94274d9c4be6db5
Eleonore Veyron <eleonore.veyron@etu.minesparis.psl.eu>
2026-09-08 11:44:41 +0200

    Canaux RGB de l'image

--- TPinfo1.py
+++ TPinfo1.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 #Defaut figures matplotlib
 plt.rcParams["figure.figsize"] = (4, 4)
 
-#Exercice
+"""     Exercice       """
+"""
 #1
 tableau = np.array([[(255,255,255) for i in range(91)] for k in range(91)])
 plt.imshow(tableau)
@@ -24,11 +25,91 @@ plt.show()
 print("RGB du 1er pixel",tableau[0][0])
 print("RGB du dernier pixel", tableau[-1][-1])
 #5
-k=0
-while k<91:
+k=0 #compteur
+while k<91: 
     for i in range(91):
         tableau[k][i]=(0,0,255)
         tableau[i][k]=(0,0,255)
     k+=10
 plt.imshow(tableau)
 plt.show()
+"""
+"""     Lecture d'une image en couleur     """
+#1
+im = plt.imread("data/les-mines.jpg")
+#2
+if not im.flags.writeable:
+    im=im.copy()
+#3
+plt.imshow(im)
+plt.show()
+#4
+print("type de l'objet", type(im))
+#5
+print("Dimension", im.ndim)
+#6
+print("Taille", im.shape[:2]) #hauteur, largeur
+#7
+print("Nb octet", im.itemsize) #octet par valeur
+#8
+print("Type pixel", im.dtype)
+#9
+print(im.max(), im.min()) #valeur max, min pixels
+#10
+im2=im[:10,:10]
+plt.imshow(im2)
+plt.show()
+
+""" Accès à des parties d'image """
+#1
+im = plt.imread("data/les-mines.jpg")
+#2
+liste=[2,5,10,20]
+for k in liste:
+    im3=im[::k,::k]
+    plt.imshow(im3)
+    plt.show()
+#3
+x0= 533//2
+y0=800//2
+for (l,c) in ((10,20),(100,200)):
+    x1=x0 - l//2
+    x2=x0 + l//2 +1
+    y1=y0 - c//2
+    y2= y0 + c//2 +1
+    im4=im[x1:x2,y1:y2]
+    plt.imshow(im4)
+    plt.show()
+
+""" Canaux RGB de l'image   """
+#1
+im = plt.imread("data/les-mines.jpg")
+#2 et 3
+rouge=im[::,::,0]
+vert=im[::,::,1]
+bleu=im[::,::,2]
+plt.imshow(rouge,map ='Reds')
+plt.show()
+plt.imshow(vert, cmap='Greens')
+plt.show()
+plt.imshow(bleu, cmap='Blues')
+plt.show()
+#5
+#Carré rose en bas à droite
+copie=im.copy()
+copie[-200:,-200:]= (219, 112, 147)
+plt.imshow(copie)
+plt.show()
+#Carré rayé rouge en bas à droite
+copie2=im.copy()
+copie2[-200:,-200]=(255,255,255)
+copie2[-200::2,-200:,0]=255
+copie2[-200::2,-200:,1]=0
+copie2[-200::2,-200:,2]=0
+plt.imshow(copie2)
+plt.show()
+#6
+plt.imshow(copie2[-200:,-200:])
+plt.show()
+
+
